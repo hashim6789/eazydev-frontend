7579bd49206b02aece948b2ad8a6560e707bdc7f
MUHAMMED HASHIM <muhammedhashim6789@gmail.com>
2025-04-03 17:35:26 +0530

    refactor : The env configuration file is changed correctly

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,18 +1,11 @@
 export const config = {
   API_BASE_URL:
     // import.meta.env.VITE_API_BASE_URL || "www.muhammedhashim.online",
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:3333",
-  CLOUDINARY_API_KEY:
-    import.meta.env.VITE_CLOUDINARY_API_KEY || "886132447392463",
-  CLOUDINARY_API_SECRET:
-    import.meta.env.VITE_CLOUDINARY_API_SECRET || "WVXBK5SJRA6ib0Tj9G6VwETNWq0",
-  CLOUDINARY_CLOUD_NAME:
-    import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "dqrcywqyg",
-  CLOUDINARY_PRESET: import.meta.env.VITE_CLOUDINARY_PRESET || "user-profile",
-  VITE_STRIPE_PK:
-    import.meta.env.VITE_STRIPE_PK ||
-    "pk_test_51QtM40FPx9xIDyIQ5jcOcxsQ1xAKdRxopM9EnG0a5RRQbOplMEMC2xYOnEQrJySkkxMScWi5jDdVgcnPK6dASCIU00QPnXGh1q",
-  GOOGLE_AUTH_CLIENT_ID:
-    import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID ||
-    "928386759524-9tflikf8cdtpuiavjnq3a65lm0sgjf55.apps.googleusercontent.com",
+    import.meta.env.VITE_API_BASE_URL || "http:localhost:3333",
+  CLOUDINARY_API_KEY: import.meta.env.VITE_CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: import.meta.env.VITE_CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_PRESET: import.meta.env.VITE_CLOUDINARY_PRESET,
+  VITE_STRIPE_PK: import.meta.env.VITE_STRIPE_PK,
+  GOOGLE_AUTH_CLIENT_ID: import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID,
 };
